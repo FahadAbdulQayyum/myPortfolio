015e1f4f3edc5f8d9ae7f269b744216099a97ade
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-03-17 22:56:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let change = document.getElementsByClassName('change');
+let color = ['orange', 'purple', 'black', 'yellow', 'pink', 'red']
+// let i = 0;
+// var varyColor;
+// while (i <= color.length) {
+//     setInterval(() => {
+//         varyColor = color[i]
+//     }, 1000)
+// }
+// console.log('change', change);
+
+// setInterval(() => {
+//     for (let i = 0; i <= 10; i++) {
+//         console.log('i', i)
+//     }
+// }, 1000)
+
+// setInterval(() => {
+//     for (let i = 0; i <= change.length; i++) {
+//         change[i].style.backgroundColor = color[i]
+//     }
+// }, 1000)
+
+// for (let i = 0; i <= change.length; i++) {
+//     console.log(
+//         change[i].style.backgroundColor = color[0]
+//     );
+// }
+
+// for (let i = 0; i <= change.length; i++) {
+//     console.log(
+//         change[i].style.backgroundColor = 'purple'
+//     );
+// }
+
+// for (let i = 0; i <= change.length; i++) {
+//     console.log(
+//         change[i].style.backgroundColor = varyColor
+//     );
+// }
